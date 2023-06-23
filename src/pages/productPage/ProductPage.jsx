2563e15cbd747eb1893029180e5345928d69f7ec
02Alexis <m_chael1012@hotmail.com>
2023-06-22 20:42:55 -0500

    finalizing productPage with its styles, it is necessary to implement a function and that's it

--- src/pages/productPage/ProductPage.jsx
+++ src/pages/productPage/ProductPage.jsx
@@ -1,44 +1,87 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import "./ProductPage.scss"
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import "./ProductPage.scss";
 
 function ProductPage() {
-  const platoSeleccionado = useSelector((store) => store.restaurantsStore.platoSeleccionado);
-  const [ingredientesSeleccionados, setIngredientesSeleccionados] = useState([]);
+  const platoSeleccionado = useSelector(
+    (store) => store.restaurantsStore.platoSeleccionado
+  );
+  const [ingredientesSeleccionados, setIngredientesSeleccionados] = useState(
+    []
+  );
+  const [quantity, setQuantity] = useState(0);
 
-  const handleCheckboxChange = (event, ingrediente) => {
+  const handleIncrease = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleCheckboxChange = (event, ingrediente, valor) => {
     if (event.target.checked) {
       setIngredientesSeleccionados([...ingredientesSeleccionados, ingrediente]);
+      setQuantity(quantity + valor);
     } else {
       const updatedIngredientes = ingredientesSeleccionados.filter(
         (selectedIngrediente) => selectedIngrediente !== ingrediente
       );
       setIngredientesSeleccionados(updatedIngredientes);
+      setQuantity(quantity - valor);
     }
   };
 
-  const ingredientesList = platoSeleccionado?.ingredientes ? Object.entries(platoSeleccionado.ingredientes).map(([clave, valor]) => (
-    <li key={clave}>
-      <label>
-        <input
-          type="checkbox"
-          value={clave}
-          checked={ingredientesSeleccionados.includes(clave)}
-          onChange={(e) => handleCheckboxChange(e, clave)}
-        />
-        {`${clave}: ${valor}`}
-      </label>
-    </li>
-  )) : null;
+  const ingredientesList = platoSeleccionado?.ingredientes
+    ? Object.entries(platoSeleccionado.ingredientes).map(([clave, valor]) => (
+        <li key={clave}>
+          <label>
+            <input
+              type="checkbox"
+              value={clave}
+              checked={ingredientesSeleccionados.includes(clave)}
+              onChange={(e) => handleCheckboxChange(e, clave, 500)}
+            />
+
+            {`${clave}: ${valor}`}
+          </label>
+        </li>
+      ))
+    : null;
 
   return (
-    <div className='productPage'>
-      <img className='dishes' src={platoSeleccionado.imagen} alt={platoSeleccionado.nombre} />
+    <div className="productPage">
+      <img
+        className="dishes"
+        src={platoSeleccionado.imagen}
+        alt={platoSeleccionado.nombre}
+      />
       <h1>{platoSeleccionado.nombre}</h1>
       <p>{platoSeleccionado.descripcion}</p>
-      <p>Additional Ingredients</p>
-      <ul>{ingredientesList}</ul>
-      <p>Selected Ingredients: {ingredientesSeleccionados.join(', ')}</p>
+      <p className="gray">Additional Ingredients</p>
+      <ul className="ingredientesList">{ingredientesList}</ul>
+      <p className="gray">
+        Selected Ingredients: {ingredientesSeleccionados.join(", ")}
+      </p>
+
+      <div className="buttons">
+        <div className="quantity-section">
+          <button className="quantity-btn" onClick={handleDecrease}>
+            -
+          </button>
+          <span className="quantity">{quantity}</span>
+          <button className="quantity-btn" onClick={handleIncrease}>
+            +
+          </button>
+        </div>
+
+        <div className="add-section">
+          <button className="add-btn">Add</button>
+          <span className="price">{platoSeleccionado.precio}</span>
+        </div>
+      </div>
     </div>
   );
 }
